48e3d7a838b43db9ba614cf42fa5117e211c2083
Lindsey B <hazardous.narayan@gmail.com>
2021-06-10 11:50:46 -0400

    Fix it all up there

--- application.rb
+++ application.rb
@@ -1,65 +1,47 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'sinatra'
-require 'sinatra/json'
-require "sinatra/base"
-require 'json'
-
-require File.join(File.dirname(__FILE__), 'environment')
+require "bundler"
+Bundler.require
 
 
-#The helper json should use the default to_json method
-set :json_encoder, :to_json
+class Application < Sinatra::Base
+  helpers do
+    def cards
+      # At first I was like meh put this into a db and then I was like you know what
+      # fuck it, file IO like this, fast enough
+      file = File.read('./tarot_interpretations.json')
+      data_hash = JSON.parse(file)
 
-error do
-  e = request.env['sinatra.error']
-  Kernel.puts e.backtrace.join("\n")
-  'Application error'
-end
-
-helpers do
-  def error code, data
-    data = {:errors => data}
-    halt code, {'Content-Type' => 'application/json'}, data.to_json
+      return data_hash["tarot_interpretations"]
+    end
   end
 
-  def cards
-    # At first I was like meh put this into a db and then I was like you know what
-    # fuck it, file IO like this, fast enough
-    file = File.read('./tarot_interpretations.json')
-    data_hash = JSON.parse(file)
-
-    return data_hash["tarot_interpretations"]
+  # return all cards
+  get '/' do
+    json cards
   end
-end
 
-# return all cards
-get '/' do
-  json cards
-end
+  # return all cards
+  get '/cards' do
+    json cards
+  end
 
-# return all cards
-get '/cards' do
-  json cards
-end
+  # return card by id
+  get '/cards/:id' do
+    id = params[:id].to_i
 
-# return card by id
-get '/cards/:id' do
-  id = params[:id]
+    json cards.detect { |card| card["rank"] == id }
+  end
 
-  json cards.detect { |card| card["rank"] == id }
-end
+  # returns n cards
+  get '/cards/:n/draw' do
+    n = params[:n].to_i
 
-# returns n cards
-get '/cards/:n/draw' do
-  n = params[:n]
+    json cards.sample(n)
+  end
 
-  json cards.sample(n)
-end
+  # find by name
+  get '/find/:name' do
+    name = params[:name]
 
-# find by name
-get '/find/:name' do
-  name = params[:name]
-
-  json cards.detect { |card| card["name"] == name }
+    json cards.detect { |card| card["name"] == name }
+  end
 end
--- config.ru
+++ config.ru
@@ -1,10 +1,3 @@
-require File.join(File.dirname(__FILE__), 'application')
+require './application'
 
-set :run, false
-
-FileUtils.mkdir_p 'log' unless File.exists?('log')
-log = File.new("log/sinatra.log", "a+")
-$stdout.reopen(log)
-$stderr.reopen(log)
-
-run Sinatra::Application
+run Application
